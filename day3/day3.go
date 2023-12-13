fd47553c2a6694cf310fb7c3b75379580d37964a
Gaël Le Moëllic <gael.lm@gmail.com>
2023-12-13 16:58:19 +0100

    day3 - part1

--- day3/day3.go
+++ day3/day3.go
@@ -5,8 +5,18 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 	"unicode"
 )
+
+type engineNumber struct {
+	numberStr    string
+	number       int
+	startIndice  int
+	endIndice    int
+	isPartNumber bool
+	line         int
+}
 
 // read each line of file and add to a slice
 func readLines(filename string) ([]string, error) {
@@ -33,7 +43,7 @@ func findEngineSymbols(lines []string) map[int]map[int]string {
 
 	for lineNb, line := range lines {
 		for charIndex, char := range line {
-			if !unicode.IsLetter(char) && !unicode.IsNumber(char) && string(char) != "." {
+			if !unicode.IsNumber(char) && string(char) != "." {
 				_, ok := matrix[lineNb]
 				if ok {
 					matrix[lineNb][charIndex] = string(char)
@@ -47,6 +57,134 @@ func findEngineSymbols(lines []string) map[int]map[int]string {
 	return matrix
 }
 
+// findEngineNumbers takes a slice of strings (lines) and identifies engine numbers
+// within those lines. It returns a map where the key is the line number, and the
+// value is a slice of engineNumber structures representing the engine numbers found
+// in that line.
+func findEngineNumbers(lines []string) ([]engineNumber, error) {
+
+	engineNumbers := []engineNumber{}
+
+	for lineNb, line := range lines {
+		var currentNumber engineNumber
+
+		for i, char := range line {
+			if unicode.IsDigit(char) {
+				if currentNumber.numberStr == "" {
+					currentNumber.line = lineNb
+					currentNumber.startIndice = i
+					currentNumber.endIndice = i
+					currentNumber.numberStr = string(char)
+				} else {
+					currentNumber.numberStr += string(char)
+					currentNumber.endIndice = i
+				}
+			} else {
+				if currentNumber.numberStr != "" {
+
+					nb, err := strconv.Atoi(currentNumber.numberStr)
+					if err != nil {
+						return nil, errors.New("failed to convert count to integer: " + err.Error())
+					}
+					currentNumber.number = nb
+
+					engineNumbers = append(engineNumbers, currentNumber)
+					currentNumber = engineNumber{}
+				}
+			}
+		}
+
+		//process end of line
+		if currentNumber.numberStr != "" {
+			nb, err := strconv.Atoi(currentNumber.numberStr)
+			if err != nil {
+				return nil, errors.New("failed to convert count to integer: " + err.Error())
+			}
+			currentNumber.number = nb
+
+			engineNumbers = append(engineNumbers, currentNumber)
+			currentNumber = engineNumber{}
+		}
+	}
+	return engineNumbers, nil
+}
+
+// function to check if a number is in an interval
+func isNumberInInterval(nb int, interval []int) bool {
+	if nb >= interval[0] && nb <= interval[1] {
+		return true
+	}
+	return false
+}
+
+func isPartNumber(eNbr engineNumber, symbolsMatrix map[int]map[int]string) bool {
+
+	//check lines
+	possibleSymbolLines := []int{eNbr.line - 1, eNbr.line, eNbr.line + 1}
+	var effectiveSymbolsLines = []int{}
+	for _, line := range possibleSymbolLines {
+		if _, ok := symbolsMatrix[line]; ok {
+			effectiveSymbolsLines = append(effectiveSymbolsLines, line)
+		}
+	}
+	if len(effectiveSymbolsLines) == 0 {
+		return false
+	}
+
+	//check indices
+	interval := []int{eNbr.startIndice - 1, eNbr.endIndice + 1}
+	for _, line := range effectiveSymbolsLines {
+		for indice := range symbolsMatrix[line] {
+			if isNumberInInterval(indice, interval) {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func getEnginPartNumbers(lines []string) ([]int, error) {
+	var partNumbers []int
+	symbols := findEngineSymbols(lines)
+	numbers, err := findEngineNumbers(lines)
+	if err != nil {
+		return []int{}, err
+	}
+
+	for _, nb := range numbers {
+		if isPartNumber(nb, symbols) {
+			partNumbers = append(partNumbers, nb.number)
+		}
+
+	}
+
+	return partNumbers, nil
+}
+
+// iterate an ints slice and sum it
+func sumInts(ints []int) int {
+	sum := 0
+	for _, i := range ints {
+		sum += i
+	}
+	return sum
+}
+
 func main() {
-	fmt.Println("Hello _")
+
+	//read input file
+	lines, err := readLines("./input.txt")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	partNumbers, err := getEnginPartNumbers(lines)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	//fmt.Println("Part numbers: ", partNumbers)
+	fmt.Println("Sum of part numbers: ", sumInts(partNumbers))
+
 }
--- day3/day3_test.go
+++ day3/day3_test.go
@@ -43,8 +43,8 @@ func TestReadLines(t *testing.T) {
 func TestFindEngineSymbols(t *testing.T) {
 	// Example input lines
 	lines := []string{
-		"Hello#123,",
-		"...Worlcd!",
+		".....#123,",
+		".........!",
 	}
 
 	// Call the function to find engine symbols
@@ -59,5 +59,196 @@ func TestFindEngineSymbols(t *testing.T) {
 	// Check if the result matches the expected result
 	if !reflect.DeepEqual(symbolMatrix, expectedMatrix) {
 		t.Errorf("Result does not match the expected matrix. Got: %v, Expected: %v", symbolMatrix, expectedMatrix)
+	}
+}
+
+func TestFindEngineNumbers(t *testing.T) {
+	// Example input lines
+	lines := []string{
+		"467..114..",
+		"...*......",
+		"..35..633.",
+		".......982",
+		".......9..",
+	}
+
+	// Call the function to find engine numbers
+	result, _ := findEngineNumbers(lines)
+
+	// Expected result based on the example input
+	expectedResult := []engineNumber{
+		{line: 0, numberStr: "467", startIndice: 0, endIndice: 2, number: 467, isPartNumber: false}, {line: 0, numberStr: "114", startIndice: 5, endIndice: 7, number: 114, isPartNumber: false},
+		{line: 2, numberStr: "35", startIndice: 2, endIndice: 3, number: 35, isPartNumber: false}, {line: 2, numberStr: "633", startIndice: 6, endIndice: 8, number: 633, isPartNumber: false},
+		{line: 3, numberStr: "982", startIndice: 7, endIndice: 9, number: 982, isPartNumber: false}, {line: 4, numberStr: "9", startIndice: 7, endIndice: 7, number: 9, isPartNumber: false}}
+
+	// Check if the result matches the expected result
+	if !reflect.DeepEqual(result, expectedResult) {
+		t.Errorf("Result does not match the expected result. Got: %v, Expected: %v", result, expectedResult)
+	}
+}
+
+func TestIsNumberInInterval(t *testing.T) {
+	// Test cases
+	testCases := []struct {
+		number   int
+		interval []int
+		expected bool
+	}{
+		{5, []int{0, 10}, true},
+		{15, []int{0, 10}, false},
+		{0, []int{0, 10}, true},
+		{10, []int{0, 10}, true},
+		{-5, []int{-10, 5}, true},
+		{-15, []int{-10, 5}, false},
+	}
+
+	// Iterate through test cases
+	for _, testCase := range testCases {
+		result := isNumberInInterval(testCase.number, testCase.interval)
+
+		// Check if the result matches the expected result
+		if result != testCase.expected {
+			t.Errorf("For number %d and interval %v, expected %t, but got %t", testCase.number, testCase.interval, testCase.expected, result)
+		}
+	}
+}
+
+func TestIsPartNumber(t *testing.T) {
+
+	symbolMatrix := map[int]map[int]string{
+		1: {3: "*"},
+		3: {6: "#"},
+		4: {3: "*"},
+	}
+	// Test cases
+	testCases := []struct {
+		engineNumber   engineNumber
+		symbolsMatrix  map[int]map[int]string
+		expectedResult bool
+	}{
+		{
+			engineNumber{"467", 467, 0, 2, false, 0},
+			symbolMatrix,
+			true,
+		},
+		{
+			engineNumber{"114", 114, 5, 7, false, 0},
+			symbolMatrix,
+			false,
+		},
+		{
+			engineNumber{"633", 633, 6, 8, false, 2},
+			symbolMatrix,
+			true,
+		},
+	}
+
+	// Iterate through test cases
+	for _, testCase := range testCases {
+		result := isPartNumber(testCase.engineNumber, testCase.symbolsMatrix)
+
+		// Check if the result matches the expected result
+		if result != testCase.expectedResult {
+			t.Errorf("For engine number %v and symbols matrix %v, expected %t, but got %t", testCase.engineNumber, testCase.symbolsMatrix, testCase.expectedResult, result)
+		}
+	}
+}
+
+func TestGetEnginPartNumbers(t *testing.T) {
+	// Test cases
+	testCases := []struct {
+		lines          []string
+		expectedResult []int
+		expectedError  error
+	}{
+		{
+			[]string{
+				"467..114..",
+				"...*......",
+				"..35..633.",
+				"......#...",
+				"617%......",
+				".....+.58.",
+				"..592.....",
+				"......755.",
+				"...$./....",
+				".664.598..",
+			},
+			[]int{467, 35, 633, 617, 592, 755, 664, 598},
+			nil,
+		},
+		{
+			[]string{
+				".......@...",
+				"........982",
+				".370.......",
+				"...*.......",
+			},
+			[]int{982, 370},
+			nil,
+		},
+		{
+			[]string{
+				"...........",
+				"........982",
+				".370.......",
+				"...%.......",
+			},
+			[]int{370},
+			nil,
+		},
+		{
+			[]string{
+				"...........",
+				"....982....",
+				"...........",
+			},
+			[]int{},
+			nil,
+		},
+		{
+			[]string{
+				"...........",
+				"......9....",
+				"+.....)...*",
+			},
+			[]int{9},
+			nil,
+		},
+	}
+
+	// Iterate through test cases
+	for _, testCase := range testCases {
+		result, err := getEnginPartNumbers(testCase.lines)
+
+		if len(testCase.expectedResult) > 0 {
+			// Check if the result matches the expected result
+			if !reflect.DeepEqual(result, testCase.expectedResult) {
+				t.Errorf("For lines %v, expected %v, but got %v", testCase.lines, testCase.expectedResult, result)
+			}
+		} else {
+			if len(result) != len(testCase.expectedResult) {
+				t.Errorf("For lines %v, expected %v, but got %v", len(testCase.lines), len(testCase.expectedResult), result)
+			}
+		}
+
+		// Check if the error matches the expected error
+		if (err != nil && testCase.expectedError == nil) || (err == nil && testCase.expectedError != nil) {
+			t.Errorf("For lines %v, expected error %v, but got %v", testCase.lines, testCase.expectedError, err)
+		}
+	}
+}
+
+func TestSumInts(t *testing.T) {
+	testCases := []struct {
+		ints           []int
+		expectedResult int
+	}{
+		{[]int{467, 35, 633, 617, 592, 755, 664, 598}, 4361},
+	}
+
+	result := sumInts(testCases[0].ints)
+	if result != testCases[0].expectedResult {
+		t.Errorf("Expected %v, but got %v", testCases[0].expectedResult, result)
 	}
 }
